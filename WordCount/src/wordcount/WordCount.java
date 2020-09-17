fe677f60f5ee0f501c32cb1d0e51e6598bceaf61
Karim <karim.rhoualem@gmail.com>
2020-09-17 11:11:55 -0400

    Added main file word count functionality to WordCount.java on feature_wordcount branch.

---
+++ WordCount/src/wordcount/WordCount.java
@@ -0,0 +1,66 @@
+package wordcount;
+
+import java.io.*;
+
+public class WordCount {
+
+    private static boolean isSpace(int c) {
+        return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+    }
+
+    public static void main(String[] args) {
+        String srcFileName = null;
+        FileInputStream fileInputStream = null;
+        BufferedReader reader = null;
+
+        if (args.length != 1) {
+            System.out.println("Usage: CharCount <src>\n");
+            return;
+        }
+
+        if (args[0] != null) {
+            try {
+                srcFileName = args[0];
+                fileInputStream = new FileInputStream((srcFileName));
+                reader = new BufferedReader(new InputStreamReader(fileInputStream));
+
+                System.out.println("Reading words in " + srcFileName + ".");
+
+                int character;
+                int nWords = 0;
+                boolean inWord = false;
+
+                while ((character = reader.read()) != -1) {
+                    if(!isSpace(character)) {
+                        if (!inWord) {
+                            inWord = true;
+                            ++nWords;
+                        }
+                    } else {
+                        inWord = false;
+                    }
+                }
+
+                System.out.println("Total words read: " + nWords + ".");
+            }
+            catch (FileNotFoundException fnfe) {
+                fnfe.printStackTrace();
+            }
+            catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    reader.close();
+                    fileInputStream.close();
+                }
+                catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
+}
