2daddc4298fc81fd265f38bdc8217848df5a5f7a
Karim <karim.rhoualem@gmail.com>
2020-09-17 15:40:53 -0400

    Added command line help options functionality in Copy.java on feature_copy branch.

--- Copy/src/copy/Copy.java
+++ Copy/src/copy/Copy.java
@@ -9,25 +9,40 @@ public class Copy {
     private static String dstFilename = "<dstFilename>";
 
     public static void main(String[] args) throws IOException {
+        if (args.length == 1) {
+            if (args[0].equals("-?") || args[0].equals("-h") || args[0].equals("-help")) {
+                System.out.println("Usage: copy <src> <dst>");
+                System.out.println("Examples:");
+                System.out.println("\tCopy file1.txt file2.txt");
+                System.out.println("\tCopy program1.java program2.java");
+            }
+            else {
+                System.out.println("Invalid arguments provided.");
+                System.out.println("Use -?, -h, or -help for help.");
+                System.out.println("Usage: Copy <src> <dst>");
+            }
+            return;
+        }
+
         // Check the number of arguments.
         // In Java, the command name 'copy' is considered as an argument.
-        if ( args.length != 2 ) {
-            System.out.println("Usage: copy <src> <dst>" );
+        if (args.length != 2) {
+            System.out.println("Usage: Copy <src> <dst>" );
             return;
         }
 
         // Check if arguments are valid, if the srcFile exists, and if can create the dstFile.
         if (args[0] != null) { // Check <src>
             srcFilename = args[0];
-            System.out.println("copy: srcFilename = '" + srcFilename + "'");
+            System.out.println("Copy: srcFilename = '" + srcFilename + "'");
             srcFile = new File(srcFilename);
             if (!srcFile.canRead()) {
-                System.out.println("copy: Cannot open srcFile '" + srcFilename + "'");
+                System.out.println("Copy: Cannot open srcFile '" + srcFilename + "'");
                 return;
             }
         } 
 		else {
-            System.out.println("copy: [OK] srcFilename = '" + srcFilename + "'");
+            System.out.println("Copy: [OK] srcFilename = '" + srcFilename + "'");
         }
 
         if (args[1] != null) { // Check <dst>
@@ -35,7 +50,7 @@ public class Copy {
             dstFile = new File(dstFilename);
         } 
 		else {
-            System.out.println("copy: [OK] dstFilename = '" + dstFilename + "'");
+            System.out.println("Copy: [OK] dstFilename = '" + dstFilename + "'");
         }
 
         FileInputStream srcStream = new FileInputStream(srcFile);
@@ -53,6 +68,6 @@ public class Copy {
         srcStream.close();
         dstStream.close();
 
-        System.out.println("copy: done.");
+        System.out.println("Copy: done.");
     }
 }
